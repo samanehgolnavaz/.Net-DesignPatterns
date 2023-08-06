866d68f4e937e81da1224d16169eff51944cc5b2
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2023-08-07 00:20:22 +0330

    Add Singletone sample

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        //Lazy<T>
+        private static readonly Lazy<Logger> LazyLogger
+            = new Lazy<Logger>(() => new Logger());
+
+        private static Logger? _instance;
+
+        public static Logger Instance
+        {
+
+            get
+            {
+                if (_instance==null)
+                {
+                    _instance=new Logger();
+                }
+                return _instance;
+            } 
+        }
+        protected Logger()
+        {
+            
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log :{message}");
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+using Singleton;
+
 Console.WriteLine("Hello, World!");
+//can not new because of protected constructor
+//var singletonTest=new Logger();
+var instance1=Logger.Instance;
+var instance2=Logger.Instance;
+if (instance1 == instance2 && instance2==Logger.Instance)
+{
+    Console.WriteLine("Instances are the same.");
+}
+instance1.Log($"Message from {nameof(instance1)}");
+//or
+instance2.Log($"Message from {nameof(instance2)}");
+//or
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+Console.ReadLine();

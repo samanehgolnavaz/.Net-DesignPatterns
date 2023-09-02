e01bd527e3ce7003c3d6598424e0582e55cb0e3b
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2023-09-02 11:27:04 +0330

    Add  Creational-Prototype Sample

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Prototype;
+
 Console.WriteLine("Hello, World!");
+var manager = new Manager("Cindy");
+var managerClone=(Manager)manager.Clone();
+Console.WriteLine($"Manager was Clones :{managerClone.Name}");
+var employee = new Employee("Kevin", manager);
+var employeeClone = (Employee)managerClone.Clone(true);
+Console.WriteLine($"Employee was Clones :{employeeClone.Name}"+
+    $"with manager{employeeClone.Manager.Name}");
+Console.ReadKey();

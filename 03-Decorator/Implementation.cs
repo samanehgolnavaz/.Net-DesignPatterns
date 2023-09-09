f3deb6713b2afcea1f9dbea55687116b795fe666
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2023-09-09 23:22:45 +0330

    Add Simple  Decorator Pattern

---
+++ 03-Decorator/Implementation.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Decorator
+{
+    /// <summary>
+    /// Component(as interface)
+    /// </summary>
+    public  interface IMailService
+    {
+        bool SendMail(string message);
+    }
+    /// <summary>
+    /// ConcreteComponent1
+    /// </summary>
+    public class CloudMailService : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine($"Message  {message} sent via {nameof(CloudMailService)}.");
+            return true;
+        }
+    }
+    /// <summary>
+    /// ConcreteComponent2
+    /// </summary>
+    public class OnPremiseMailService : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine($"Message  {message} sent via {nameof(OnPremiseMailService)}.");
+            return true;
+        }
+    }
+    /// <summary>
+    /// Decorator
+    /// </summary>
+    public abstract class MailServiceDecoratorBase: IMailService
+    {
+        private readonly IMailService _mailService;
+        public MailServiceDecoratorBase(IMailService mailService)
+        {
+
+            _mailService = mailService;
+
+        }
+        public virtual bool SendMail(string message)
+        {
+            return _mailService.SendMail(message);
+        }
+    }
+    /// <summary>
+    /// ConcreteDecorator1
+    /// </summary>
+    public class StatisticDecorator : MailServiceDecoratorBase
+    {
+        public StatisticDecorator(IMailService mailService) : base(mailService)
+        {
+        }
+        public override bool SendMail(string message)
+        {
+            //Fake collecting statistics
+            Console.WriteLine($"collecting statistics in {nameof(StatisticDecorator)}");
+            return base.SendMail(message);
+        }
+    }
+    public class MessageDatabaseDecorator : MailServiceDecoratorBase
+    {
+        public List<string> SendMessages { get; private set; } = new List<string>();
+        public MessageDatabaseDecorator(IMailService mailService) : base(mailService)
+        {
+        }
+        public override bool SendMail(string message)
+        {
+            if (base.SendMail(message))
+            {
+                //store send message
+                SendMessages.Add(message);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 03-Decorator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03_Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //instantiate mail services
+            var cloudMailService=new CloudMailService();
+            cloudMailService.SendMail("Hi there");
+            var onPermiseMailService=new OnPremiseMailService();
+            onPermiseMailService.SendMail("Hi there.");
+            // add behavior
+            var statisticsDecorator = new StatisticDecorator(cloudMailService);
+            statisticsDecorator.SendMail($"Hi there via {nameof(StatisticDecorator)}wrapper.");
+            //add behavior
+            var messageDatabaseDecorator = new MessageDatabaseDecorator(onPermiseMailService);
+            messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)}wrapper,message 1.");
+             messageDatabaseDecorator = new MessageDatabaseDecorator(onPermiseMailService);
+            messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)}wrapper,message 2.");
+
+            foreach (var message in messageDatabaseDecorator.SendMessages)
+            {
+                Console.WriteLine($"Stored message: \"{message}");
+            }
+            Console.ReadKey();
+      
+        }
+    }
+}

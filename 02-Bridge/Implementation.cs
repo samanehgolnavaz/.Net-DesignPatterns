f22048b337dea5fe6bf14dd001f90f6ed8b7d2fa
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2023-09-09 19:33:57 +0330

    Add Simple  Bridge Pattern

---
+++ 02-Bridge/Implementation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Bridge
+{
+    /// <summary>
+    /// Abstraction
+    /// </summary>
+    public abstract class Menu
+    {
+        public readonly ICopoun _copoun = null;
+        public abstract int CaclculatePrice();
+        public Menu(ICopoun copoun)
+        {
+            _copoun = copoun;
+        }
+    }
+    /// <summary>
+    /// RefinedAbstraction
+    /// </summary>
+    public class VegetarianMenu : Menu
+    {
+        public VegetarianMenu(ICopoun copoun) : base(copoun)
+        {
+        }
+        public override int CaclculatePrice()
+        {
+            return 20 - _copoun.CopounValue;
+        }
+    }
+    /// <summary>
+    /// RefinedAbstraction
+    /// </summary>
+    public class MeatBaseMenu : Menu
+    {
+        public MeatBaseMenu(ICopoun copoun) : base(copoun)
+        {
+        }
+        public override int CaclculatePrice()
+        {
+            return 30 - _copoun.CopounValue;
+        }
+    }
+    /// <summary>
+    /// Implemetor
+    /// </summary>
+    public interface ICopoun
+    {
+         int CopounValue { get; }
+    }
+    /// <summary>
+    /// ConcreteImplementor
+    /// </summary>
+    public class NoCoupon : ICopoun
+    {
+        public int CopounValue { get => 0; }
+    }
+    /// <summary>
+    /// ConcreteImplementor
+    /// </summary>
+    public class OneEuroCopoun : ICopoun
+    {
+        public int CopounValue { get => 1; }
+
+    }
+    /// <summary>
+    /// ConcreteImplementor
+    /// </summary>
+    public class TwoEuroCopoun : ICopoun
+    {
+        public int CopounValue { get => 2; }
+
+    }
+
+}
---
+++ 02-Bridge/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02_Bridge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var noCoupon=new NoCoupon();
+            var oneEuroCoupon = new OneEuroCopoun();
+            var meatBaseMenu=new MeatBaseMenu(noCoupon);
+            Console.WriteLine($"Meat based menu, no copun:{meatBaseMenu.CaclculatePrice()}euro.");
+            meatBaseMenu = new MeatBaseMenu(oneEuroCoupon);
+            Console.WriteLine($"Meat based menu, one euro copun:{meatBaseMenu.CaclculatePrice()}euro.");
+            var vegetarianMenu=new VegetarianMenu(noCoupon);
+            Console.WriteLine($"Vegetarian menu, no copun:{vegetarianMenu.CaclculatePrice()}euro.");
+             vegetarianMenu = new VegetarianMenu(oneEuroCoupon);
+            Console.WriteLine($"Vegetarian menu, one euro copun:{vegetarianMenu.CaclculatePrice()}euro.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
